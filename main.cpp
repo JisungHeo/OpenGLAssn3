c60b208e79f6e0a594f97a7a16eacba7278b72cf
허지성(컴퓨터공학과) <jsheo@postech.ac.kr>
2018-11-15 21:46:46 +0900

    third-person complete

--- main.cpp
+++ main.cpp
@@ -21,41 +21,77 @@ GLuint VertexArrayID;
 glm::mat4 MVP;
 GLuint vertexbuffer;
 unsigned int dummy_obj_size;
-
+bool third_person_view = false;
 
 //Player player(0.0f,0.0f);
 using namespace std; 
 
 glm::mat4 cameraMove() {
-	float dx=0.0f;
+	float dx=0.0f;//offset for the point looked at
 	float dy=0.0f;
-	float dhx = 0.0f;
+	float dhx = 0.0f;//offset for head position
 	float dhy = 0.0f;
+	float dtx = 0.0f;
+	float dty = 0.0f;//offset for third-person view
+	float dtz = 0.0f;
+	if (third_person_view)
+		dtz = 400.0f;
+	else
+		dtz = 0.0f;
+
 	if (Player::player.direction == 0.0f) {
 		dx = 1000.0f;
 		dy = 0.0f;
 		dhx = 20.0f;
 		dhy = 0.0f;
+		if (third_person_view) {
+			dtx = -1000.0f;
+			dty = 0.0f;
+		}
+		else {
+			dtx = dty = 0.0f;
+		}
 	}
 	else if (Player::player.direction == 90.0f) {
 		dx = 0.0f;
 		dy = 1000.0f;
 		dhx = 0.0f;
 		dhy = 20.0f;
+		if (third_person_view) {
+			dtx = 0.0f;
+			dty = -1000.0f;
+		}
+		else {
+			dtx = dty = 0.0f;
+		}
 	}
 	else if (Player::player.direction == 180.0f) {
 		dx = -1000.0f;
 		dy = 0.0f;
 		dhx = -20.0f;
 		dhy = 0.0f;
+		if (third_person_view) {
+			dtx = 1000.0f;
+			dty = 0.0f;
+		}
+		else {
+			dtx = dty = 0.0f;
+		}
 	}
 	else if (Player::player.direction == 270.0f) {
 		dx = 0.0f;
 		dy = -1000.0f;
 		dhx = 0.0f;
 		dhy = -20.0f;
+		if (third_person_view) {
+			dtx = 0.0f;
+			dty = 1000.0f;
+		}
+		else {
+			dtx = dty = 0.0f;
+		}
 	}
-	return glm::lookAt(glm::vec3(Player::player.x+dhx, Player::player.y+dhy, 170.0f),
+	return glm::lookAt(glm::vec3(Player::player.x+dhx+dtx, Player::player.y+dhy+dty, 170.0f+dtz),
 		glm::vec3(Player::player.x+dx, Player::player.y+dy, 169.0f),
 		glm::vec3(0.0f, 0.0f, 1.0f));
 }
@@ -94,8 +130,11 @@ void keyboard(unsigned char key, int x, int y) {
 	case 'd':
 		Player::player.rotate(-90.0f);
 		break;
+
+	case 'q':
+		third_person_view = !third_person_view;
+		break;
 	}
-	
 	printf("%f\n", Player::player.direction);
 	glutPostRedisplay();
 }
