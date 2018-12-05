fa0008a94a957144042af7cb1a9c5e0e871803ca
jisoo kim <jisoo97@postech.ac.kr>
2018-12-05 14:22:50 +0900

    shader

--- myVS.glsl
+++ myVS.glsl
@@ -1,4 +1,4 @@
-#version 440 core
+/*#version 440 core
 // myVS.glsl
 
 layout (location = 0) in vec3 vertexPosition;
@@ -9,5 +9,20 @@ uniform mat4 Model;
 void main()
 {
     gl_Position = Projection * View * Model * vec4(vertexPosition, 1);
-}
+}*/
  
+in vec4 vPosition;
+in vec3 vNormal;
+
+out vec3 fN;
+
+uniform mat4 View;
+uniform mat4 Model;
+uniform mat4 Projection;
+uniform vec4 fragmentColor;
+
+void main()
+{
+	fN = vNormal;
+	gl_Position = Projection*View*Model*vPosition;  //**gl position vs fE Â÷ÀÌ
+}
